22309758bb4464f9c853d3efa17684457fe98923
chupishka <mrbubon1337@gmail.com>
2025-04-05 23:26:34 +0400

    перенес в один класс все, сделал цикл создания файла. надо допилить массив хранящий значения.

--- Table.py
+++ Table.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
+
 class Table(Tk):
     def __init__(self,rows,col):
         super().__init__()
@@ -11,20 +12,40 @@ class Table(Tk):
         # self.lbl.pack()
         # self.lbl1 = ttk.Label(self,text=col)
         # self.lbl1.pack()
+        frame_table = ttk.Frame(self,borderwidth=10,padding=[10,10],relief=SOLID)
+        table_menu = Menu(self)
+        file_menu = Menu(self)
+        file_menu.add_cascade(label="New book",command= lambda x = self:Table.new_book(self))
+        file_menu.add_cascade(label="Save book")
+
+        table_menu.add_cascade(label="File",menu=file_menu)
+        table_menu.add_cascade(label="Edit")
+        self.config(menu = table_menu)
         self.columns = []
         for i in range(int(rows)+1):
+            dat=[]
             for j in range(int(col)+1):
+
                 if i==0 and j== 0:
                     continue
                 if i==0:
-                    lbl = ttk.Label(self,text = "Var"+str(j))
+                    lbl = ttk.Entry(frame_table,text = "Var"+str(j))
+                    lbl.insert(0,"Var"+str(j))
                     lbl.grid(row = i,column=j)
                 elif j==0:
-                    lbl = ttk.Label(self,text = str(i))
+                    lbl = ttk.Label(frame_table,text = str(i))
                     lbl.grid(row = i,column=j)
                 else:
-                    entry = ttk.Entry(self,text=str(i)+str(j))
+                    entry = ttk.Entry(frame_table,text=str(i)+str(j))
                     entry.grid(row = i,column=j)
+        frame_table.pack()
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
+    def new_book(self):
+        # from main import Create
+        # new_create = Create()
+        return
+
+
 
         # for i in range(int(col)):
         #     self.columns += ["Var"+str(i)]
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
+from symtable import Function
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-from Table import Table
 
-# def create():
-#
-#     labl = ttk.Label(text="jopa")
-#     labl.pack()
-#     root.mainloop()
-# root = Tk()
-# root.geometry("500x200")
-# lbl1 = ttk.Label(text="Введите кол-во столбцов:")
-# edit_col = ttk.Entry()
-# lbl2 = ttk.Label(text="Введите кол-во строк:")
-#
-# edit_raws = ttk.Entry()
-# btn_create = ttk.Button(text="Создать",command=create)
-# lbl1.place(x="10",y="10")
-# edit_col.place(x = "200",y="10")
-# lbl2.place(x="10",y="40")
-# edit_raws.place(x = "200",y="40")
-# btn_create.pack(anchor = "s",side = 'bottom',pady = "20")
-# root.mainloop()
 class Create:
     def __init__(self):
         x = self
@@ -39,8 +20,60 @@ class Create:
         btn_create.pack(anchor="s", side='bottom', pady="20")
         self.root.mainloop()
     def create_table(self):
-        new_table = Table(self.edit_raws.get(),self.edit_col.get())
+        new_table = Book(self.edit_raws.get(),self.edit_col.get())
         self.root.destroy()
+        del self
+
+
+
+
+class Book(Tk):
+    def __init__(self,rows,col):
+        super().__init__()
+
+        self.geometry("500x500")
+
+        frame_table = ttk.Frame(self,borderwidth=3)
+        table_menu = Menu(self)
+        file_menu = Menu(self)
+        file_menu.add_cascade(label="New book",command= lambda x = self:Book.new_book(self))
+        file_menu.add_cascade(label="Save book")
+
+        table_menu.add_cascade(label="File",menu=file_menu)
+        table_menu.add_cascade(label="Edit")
+        table_menu.add_cascade(label="Functions")
+        self.config(menu = table_menu)
+        self.columns = []
+        self.data = [[]]
+        for i in range(int(rows)+1):
+            for j in range(int(col)+1):
+                if i==0 and j== 0:
+                    continue
+                if i==0:
+                    lbl = ttk.Entry(frame_table,text = "Var"+str(j),justify=RIGHT)
+                    lbl.insert(0,"Var"+str(j))
+                    lbl.grid(row = i,column=j)
+                elif j==0:
+                    lbl = ttk.Label(frame_table,text = str(i),justify=RIGHT)
+                    lbl.grid(row = i,column=j)
+                else:
+                    entry = ttk.Entry(frame_table,text=str(i)+str(j),justify=RIGHT)
+                    entry.grid(row = i,column=j)
+        frame_table.pack(anchor = "nw")
+
+    def new_book(self):
+
+        new_create = Create()
+
+    def function1(self):
+        functiontk = Tk()
+
+
+# class Function1(Tk):
+#     def __init__(self,vars):
+#         super().__init__()
+
+
 
 
 
